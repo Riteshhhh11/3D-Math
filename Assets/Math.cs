2dc3d1f560a2b56b60856f483d138e2721b2480f
Riteshhhh <riteshranjan785@gmail.com>
2026-02-19 01:04:15 +0000

    Working with the computation of dot product without cos

--- Assets/Math.cs
+++ Assets/Math.cs
@@ -4,6 +4,7 @@ public class Math : MonoBehaviour
 {
     public Transform A;
     public Transform B;
+    public float dotValue;
     private void OnDrawGizmos()
     {
         Vector2 a = A.position; 
@@ -22,7 +23,10 @@ public class Math : MonoBehaviour
         Vector2 aNorm = a.normalized; //quick and easy way to normalize a vector, it returns a new vector with the same direction but a magnitude of 1
         Gizmos.color = Color.red;
         Gizmos.DrawSphere(aNorm, 0.6f); //drawing normalized version of A, it should be a unit vector pointing in the same direction as A
-    
+
+        //Scalar Projection
+       dotValue = Vector2.Dot(aNorm, b); //aNorm is normalized and b is non normalized
+        
     }
 
 }
